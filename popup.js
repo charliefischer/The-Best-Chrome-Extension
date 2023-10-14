5b06748bfcfe968816f0e44539f70a3c2f14bb2b
charlie <charlie.a.fischer@gmail.com>
2023-10-13 17:29:28 -0700

    push a script to DOM added in commment

---
+++ popup.js
@@ -0,0 +1,10 @@
+console.log("This is a popup!")
+
+
+
+//    chrome.tabs.executeScript(null, {
+//   code: 'var config = ' + JSON.stringify(getKeywords)
+// }, function() {
+//     chrome.tabs.executeScript(null, {file: 'custom.js'});
+// });
+// execute a script - this will be needed to change the dom
